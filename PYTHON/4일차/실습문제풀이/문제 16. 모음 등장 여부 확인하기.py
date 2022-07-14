93c5e2ea2a58a58ca2c7969d1226464963ca5130
danny7128 <danny7128@gmail.com>
2022-07-14 15:26:28 +0900

    실습문제 풀이 4일차

---
+++ PYTHON/4일차/실습문제풀이/문제 16. 모음 등장 여부 확인하기.py
@@ -0,0 +1,12 @@
+# 문자열 word가 주어질 때, 해당 문자열에서 모음의 갯수를 출력하시오.
+# 모음 : a, e, i, o, u 
+# count() 사용 금지
+
+word = input()
+cnt = 0
+
+for words in word:
+    if words in ['a', 'e', 'i', 'o', 'u']:
+        cnt +=1
+   
+print(cnt)
