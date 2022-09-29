2a05534ce24d5ca6d0e3a60d2b05ffd830acb6a7
danny7128 <danny7128@gmail.com>
2022-09-29 18:52:27 +0900

    2022-09-29-django-07

---
+++ 06_django/실습/날짜/0929/test1/crud/todos/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+def index(request):
+    _todos = Todo.objects.all().order_by("id")
+    context = {
+        "todos": _todos,
+    }
+
+    return render(request, "todos/index.html", context)
+
+
+def create(request):
+    content = request.GET.get("content___")
+    priority = request.GET.get("priority")
+    deadline = request.GET.get("deadline")
+    Todo.objects.create(content=content, priority=priority, deadline=deadline)
+
+    """
+    리다이렉트
+    1. 사용자가 create 요청
+    2. 서버에서 index 요청
+    3. 사용자에게는 index 응답
+    """
+
+    return redirect("todos:index")
+
+
+def delete(request, todo_pk):
+    todo = Todo.objects.get(pk=todo_pk)
+    todo.delete()
+
+    return redirect("todos:index")
+
+
+def update(request, pk):
+    todo = Todo.objects.get(pk=pk)
+
+    # True -> False
+    # False -> True
+    todo.completed = not todo.completed
+
+    todo.save()
+    return redirect("todos:index")
